cca4ca13084c34c1ccf21436eab7a83b186940c2
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-07-26 11:45:49 -0400

    recipe: Bump build number and rebuild with new OpenBLAS.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,4 +4,10 @@ if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
+# Use only 1 thread with OpenBLAS to avoid timeouts on CIs.
+# This should have no other affect on the build. A user
+# should still be able to set this (or not) to a different
+# value at run-time to get the expected amount of parallelism.
+export OPENBLAS_NUM_THREADS=1
+
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
